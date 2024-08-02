59048e3217792d69a74e1a567d91dd0e4eb533da
Said Sef <6002187+saidsef@users.noreply.github.com>
2024-08-03 00:27:06 +0400

    chore: version bump (#180)
    * chore: version bump
    
    * feat: add eslint v9 config
    
    Signed-off-by: Said Sef <saidsef@gmail.com>
    
    ---------
    
    Signed-off-by: Said Sef <saidsef@gmail.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+export default [
+  {
+    files: ["libs/**"],
+    ignores: [],
+    rules: {
+      semi: "error",
+      "for-direction": "error",
+      "getter-return": "error",
+      "no-compare-neg-zero": "error",
+      "no-dupe-args": "error",
+      "no-dupe-else-if": "error",
+      "no-duplicate-imports": "error",
+      "no-irregular-whitespace": "error",
+      "no-self-assign": "error",
+      "no-setter-return": "error",
+      "no-unused-vars": "error",
+      "prefer-const": "error",
+      "valid-typeof": "error",
+    }
+  }
+];
